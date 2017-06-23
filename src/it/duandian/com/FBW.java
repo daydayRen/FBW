9d4fa7cbd31c8c55613234b76f589e514db26e4c
daydayRen0 <1209050193@qq.com>
2017-06-23 19:41:12 +0800

    修复bug

--- src/it/duandian/com/FBW.java
+++ src/it/duandian/com/FBW.java
@@ -4,19 +4,11 @@ import java.util.Scanner;
 
 /**
  * 
- * 一
- *  根据题目要求输入三个个位数 判断是否输入错误 
- *  
- *   补充：
- *     由题可知要求6权重最高 
- *     优先判断6  
- *     
- * 二 
- * 	首个特殊子最为特殊 特殊判断 
- * 三
- * 	 判断是否包含单个特殊数字 
- * 四 
- * 	判断倍数
+ * 一 根据题目要求输入三个个位数 判断是否输入错误
+ * 
+ * 补充： 由题可知要求6权重最高 优先判断6
+ * 
+ * 二 首个特殊子最为特殊 特殊判断 三 判断是否包含单个特殊数字 四 判断倍数
  * 
  * @author oneday
  *
@@ -32,26 +24,43 @@ public class FBW {
 		int c = sc.nextInt();
 		judge(c);
 		sc.close();
+
 		// 二特殊中的特殊判断
 		for (int i = 1; i <= 100; i++) {
-			if (Integer.toString(i).indexOf(Integer.toString(a)) > 0) {
-				System.out.print("Fizz");
-			} else if (Integer.toString(i).indexOf(Integer.toString(b)) > 0) {
-				System.out.print("Fizz");
-			} else if (Integer.toString(i).indexOf(Integer.toString(c)) > 0) {
-				System.out.print("Fizz");
-			}
+			StringBuffer sb = new StringBuffer();// 用于类似包含两个数字倍数的数字
+			if (Integer.toString(i).indexOf(Integer.toString(a)) >= 0) {//判断是否包含第一个特殊数字
+				System.out.println("Fizz");
+//				if (Integer.toString(i).indexOf(Integer.toString(b)) > 0
+//						|| Integer.toString(i).indexOf(Integer.toString(c)) > 0)
+					//System.out.print("Fizz");
 
-			else {
-				System.out.print(i);
-			}
+			} else {
+				// 判断倍数
+				if (i % a == 0 || i % b == 0 || i % c == 0) {
+					if (i % a == 0) {
+						sb.append("Fizz");
+					}
+					if (i % b == 0) {
+						sb.append("Buzz");
+					}
+					if (i % c == 0) {
+						sb.append("Whizz");
+					}
+					System.out.println(sb);
 
-			System.out.println();
+				}
+
+				else {
+					System.out.println(i);
+				}
+			}
+			
 		}
 	}
 
 	/**
 	 * 判断数字的正确性
+	 * 
 	 * @param n
 	 */
 	public static void judge(int n) {
